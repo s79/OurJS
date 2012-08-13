93c06885350d62d16f10d627fab366eff7ba65e2
s79 <sundongguo@gmail.com>
2012-08-13 10:11:49 +0800

    0813

--- dev.js
+++ dev.js
@@ -21,7 +21,7 @@
     scripts.push('<script src="/framework/src/integrated/json2.js"></script>');
   }
   if (document.documentElement.currentStyle && !document.documentElement.currentStyle.minWidth) {
-    scripts.push('<script src="/framework/src/integrated/console.js"></script>');
+    scripts.push('<script src="/framework/src/addons/console.js"></script>');
   }
   document.write(scripts.join('\n'));
 })();
